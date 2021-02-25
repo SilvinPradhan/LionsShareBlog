710ff1de68eda792059ab6cfa82f0ecffa92f03f
Silvin Pradhan <silvinpradhan95@gmail.com>
2021-02-25 10:23:33 -0600

    setup for getting and setting cookie, remove cookie using js-cookie, and set the user data to localStorage

--- client/actions/auth.js
+++ client/actions/auth.js
@@ -1,5 +1,6 @@
 import fetch from 'isomorphic-fetch';
 import { API } from '../config';
+import cookie from 'js-cookie';
 
 export const signup = (user) => {
 	return fetch(`${API}/signup`, {
@@ -29,4 +30,64 @@ export const signin = (user) => {
 			return res.json();
 		})
 		.catch((err) => console.log(err));
+};
+
+// Set Cookie
+export const setCookie = (key, value) => {
+	// check if the process is running in the client side
+	if (process.browser) {
+		cookie.set(key, value, {
+			expires: 1,
+		});
+	}
+};
+
+export const removeCookie = (key) => {
+	if (process.browser) {
+		cookie.remove(key, {
+			expires: 1,
+		});
+	}
+};
+
+// Get Cookie
+export const getCookie = (key) => {
+	if (process.browser) {
+		cookie.get(key);
+	}
+};
+
+// LocalStorage
+export const setLocalStorage = (key, value) => {
+	if (process.browser) {
+		localStorage.setItem(key, JSON.stringify(value));
+	}
+};
+
+export const removeLocalStorage = (key) => {
+	if (process.browser) {
+		localStorage.removeItem(key);
+	}
+};
+
+/* Middleware Setup  */
+// Authenticate user by pass data to cookie and localStorage
+export const authenticate = (data, next) => {
+	setCookie('token', data.token);
+	setLocalStorage('user', data.user);
+	next();
+};
+
+// Get the authenticated user data from localStorage
+export const isAuthenticated = () => {
+	if (process.browser) {
+		const cookieChecked = getCookie('token');
+		if (cookieChecked) {
+			if (localStorage.getItem('user')) {
+				return JSON.parse(localStorage.getItem('user'));
+			} else {
+				return false;
+			}
+		}
+	}
 };
