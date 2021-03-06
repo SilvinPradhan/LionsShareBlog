c70ca00863c40cee8316628cc0efccfef6d80367
Silvin Pradhan <silvinpradhan95@gmail.com>
2021-03-06 13:41:07 -0600

    building admin and user dashboard

---
+++ client/pages/admin/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+import Link from 'next/link';
+
+const AdminIndex = () => {
+	return <Layout>Admin Dashboard</Layout>;
+};
+
+export default AdminIndex;
---
+++ client/pages/user/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+import Link from 'next/link';
+
+const UserIndex = () => {
+	return <Layout>User Dashboard</Layout>;
+};
+
+export default UserIndex;
